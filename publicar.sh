a429c0e951d27e7ad623add031b73ee74631cc0e
Harry Wong <hwong23@gmail.com>
2020-12-16 20:00:41 -0500

    publicar html

--- publicar.sh
+++ publicar.sh
@@ -1,7 +1,7 @@
 git fetch
 git add .
 git commit -m "$1"
-grep "1\.11" *.xml -n > ../README.md
+grep dc:identifier *.xml > ../README.md
 git log -n 1 --pretty='format:%C(auto)%h %s %an %ad' >> ../README.md
 
 
