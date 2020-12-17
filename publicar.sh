db258be1a1be3cad214582108616265d73b30643
Harry Wong <hwong23@gmail.com>
2020-12-16 19:52:19 -0500

    APP. SNC. Transporte e Integración

--- publicar.sh
+++ publicar.sh
@@ -1,7 +1,8 @@
 git fetch
 git add .
 git commit -m "$1"
-git log -n 1 --pretty='format:%C(auto)%h %s %an %ad' > ../README.md
+grep "1\.11" *.xml -n > ../README.md
+git log -n 1 --pretty='format:%C(auto)%h %s %an %ad' >> ../README.md
 
 
 git push
