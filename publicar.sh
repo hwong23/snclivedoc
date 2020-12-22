6648d28fc94a7b70c0e651ae454557d447e55c4d
Harry Wong <hwong23@gmail.com>
2020-12-21 21:42:54 -0500

    rm

--- publicar.sh
+++
@@ -1,9 +0,0 @@
-git fetch
-git add .
-git commit -m "$1"
-grep dc:identifier *.xml > ../README.md
-git log -n 1 --pretty='format:%C(auto)%h %s %an %ad' >> ../README.md
-git show --name-only -n 1 >> ../README.md
-
-
-git push
